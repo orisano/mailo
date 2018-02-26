ca3f306494d145d181c24be674141129574bef2b
Nao YONASHIRO <owan.orisano@gmail.com>
2018-02-27 02:34:26 +0900

    feat: initial commit

--- jpmail.go
+++ jpmail.go
@@ -1,43 +1,76 @@
 package jpmail
 
 import (
-	"bufio"
+	"encoding/base64"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"mime"
+	"mime/quotedprintable"
 	"net/mail"
-	"net/textproto"
 	"strings"
 
-	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/encoding/ianaindex"
 	"golang.org/x/text/transform"
 )
 
 var addrParser = &mail.AddressParser{
 	WordDecoder: &mime.WordDecoder{
-		CharsetReader: func(charset string, input io.Reader) (io.Reader, error) {
-			switch {
-			case strings.EqualFold(charset, "iso-2022-jp"):
-				return transform.NewReader(input, japanese.ISO2022JP.NewDecoder()), nil
-			case strings.EqualFold(charset, "Windows-31J"), strings.EqualFold(charset, "Shift_JIS"):
-				return transform.NewReader(input, japanese.ShiftJIS.NewDecoder()), nil
-			default:
-				return nil, fmt.Errorf("charset not supported: %q", charset)
-			}
-		},
+		CharsetReader: charsetReader,
 	},
 }
 
-func ReadMessage(r io.Reader) (*mail.Message, error) {
-	tp := textproto.NewReader(bufio.NewReader(r))
+func charsetReader(charset string, input io.Reader) (io.Reader, error) {
+	if strings.EqualFold(charset, "CP932") {
+		charset = "Shift_JIS"
+	}
+	enc, err := ianaindex.IANA.Encoding(charset)
+	if err != nil {
+		return nil, fmt.Errorf("charset not supported: %q: %v", charset, err)
+	}
 
-	hdr, err := tp.ReadMIMEHeader()
+	return transform.NewReader(input, enc.NewDecoder()), nil
+}
+
+func ParseAddress(address string) (*mail.Address, error) {
+	return addrParser.Parse(address)
+}
+
+func ParseAddressList(list string) ([]*mail.Address, error) {
+	return addrParser.ParseList(list)
+}
+
+func ReadBody(msg *mail.Message) ([]byte, error) {
+	contentType := msg.Header.Get("Content-Type")
+	mediaType, params, err := mime.ParseMediaType(contentType)
 	if err != nil {
 		return nil, err
 	}
+	if mediaType != "text/plain" {
+		return nil, fmt.Errorf("unsupported media type: %q", mediaType)
+	}
 
-	return &mail.Message{
-		Header: mail.Header(hdr),
-		Body:   tp.R,
-	}, nil
+	r := msg.Body
+	encoding := msg.Header.Get("Content-Transfer-Encoding")
+	if encoding != "" {
+		switch {
+		case strings.EqualFold(encoding, "quoted-printable"):
+			r = quotedprintable.NewReader(r)
+		case strings.EqualFold(encoding, "base64"):
+			r = base64.NewDecoder(base64.StdEncoding, r)
+		case strings.EqualFold(encoding, "7bit"), strings.EqualFold(encoding, "8bit"):
+		default:
+			return nil, fmt.Errorf("unsupported encoding")
+		}
+	}
+
+	if cs, ok := params["charset"]; ok {
+		rr, err := charsetReader(cs, r)
+		if err != nil {
+			return nil, err
+		}
+		r = rr
+	}
+
+	return ioutil.ReadAll(r)
 }
