ebfb27a1863852aa0dc387ad7421fca3121ad0b0
Nao YONASHIRO <owan.orisano@gmail.com>
2018-03-13 10:04:43 +0900

    wip!

--- mailo.go
+++ mailo.go
@@ -6,9 +6,11 @@ import (
 	"io"
 	"io/ioutil"
 	"mime"
+	"mime/multipart"
 	"mime/quotedprintable"
 	"net/mail"
 	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	"golang.org/x/text/encoding/ianaindex"
@@ -86,4 +88,105 @@ func ReadBody(msg *mail.Message) (b []byte, err error) {
 		r = cr
 	}
 	return ioutil.ReadAll(r)
+}
+
+type Message struct {
+	Date    time.Time
+	From    *mail.Address
+	To      []*mail.Address
+	Subject string
+
+	Text io.Reader
+	HTML io.Reader
+
+	Resources   map[string]io.Reader
+	Attachments map[string]io.Reader
+}
+
+func ReadMessage(r io.Reader) (*Message, error) {
+	msg, err := mail.ReadMessage(r)
+	if err != nil {
+		return nil, err
+	}
+
+	contentType := msg.Header.Get("Content-Type")
+	if contentType == "" {
+		contentType = "text/plain; charset=us-ascii"
+	}
+	mediaType, params, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse content-type")
+	}
+
+	switch mediaType {
+	case "multipart/mixed":
+		return readMixed(msg.Body, params["boundary"])
+	case "multipart/related":
+		return readRelated(msg.Body, params["boundary"])
+	case "multipart/alternative":
+		return readAlternative(msg.Body, params["boundary"])
+	case "text/html":
+
+	case "text/plain":
+	}
+}
+
+func readMixed(r io.Reader, boundary string) (*Message, error) {
+	mp := multipart.NewReader(r, boundary)
+	for {
+		part, err := mp.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+}
+
+func readRelated(r io.Reader, boundary string) (*Message, error) {
+
+}
+
+func readAlternative(r io.Reader, boundary string) (*Message, error) {
+
+}
+
+func ReadText(msg *mail.Message) (b []byte, err error) {
+	encoding := msg.Header.Get("Content-Transfer-Encoding")
+	if encoding == "" {
+		encoding = "7BIT"
+	}
+	r := msg.Body
+	switch {
+	case strings.EqualFold(encoding, "quoted-printable"):
+		r = quotedprintable.NewReader(r)
+	case strings.EqualFold(encoding, "base64"):
+		r = base64.NewDecoder(base64.StdEncoding, r)
+		defer func() {
+			if err == io.ErrUnexpectedEOF {
+				err = nil
+			}
+		}()
+	case strings.EqualFold(encoding, "7bit"), strings.EqualFold(encoding, "8bit"):
+	default:
+		return nil, fmt.Errorf("unsupported encoding: %q", encoding)
+	}
+
+	contentType := msg.Header.Get("Content-Type")
+	if contentType == "" {
+		contentType = "text/plain; charset=us-ascii"
+	}
+	mediaType, params, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse content-type")
+	}
+	if cs, ok := params["charset"]; ok {
+		cr, err := charsetReader(cs, r)
+		if err != nil {
+			return nil, err
+		}
+		r = cr
+	}
+	return nil, nil
 }
